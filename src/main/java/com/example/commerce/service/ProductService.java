7b7672cb0e0ba97c4fed7c5250a80f5dafd64570
Hyoeun Jung <jungsand1325@gmail.com>
2025-09-11 16:09:09 +0900

    feat : 상품수정 서비스 로직 구현

--- src/main/java/com/example/commerce/service/ProductService.java
+++ src/main/java/com/example/commerce/service/ProductService.java
@@ -1,8 +1,10 @@
 package com.example.commerce.service;
 
 import com.example.commerce.dto.ProductCreateRequest;
+import com.example.commerce.dto.ProductUpdateRequest;
 import com.example.commerce.entity.Product;
 import com.example.commerce.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,5 +25,19 @@ public class ProductService {
                 .build();
 
         return productRepository.save(product);
+    }
+
+    //상품수정
+    @Transactional
+    public Product updateProduct(Long productId, ProductUpdateRequest productUpdateRequest) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new EntityNotFoundException("ID에 해당하는 상품을 찾을수 없음: " + productId));
+
+        product.update(productUpdateRequest.getName(),
+                       productUpdateRequest.getPrice(),
+                        productUpdateRequest.getStock(),
+                        productUpdateRequest.getDescription());
+        return product;
+
     }
 }
