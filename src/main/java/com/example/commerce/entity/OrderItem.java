5dbe412b466728aea8332adc4f655d16063cb5be
Hyoeun Jung <jungsand1325@gmail.com>
2025-09-18 13:51:31 +0900

    feat :  주문생성을 위한 기본 엔티티, enum 추가

---
+++ src/main/java/com/example/commerce/entity/OrderItem.java
@@ -0,0 +1,36 @@
+package com.example.commerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Column(nullable = false)
+    private Long productId;
+    @Column(nullable = false)
+    private String productName;
+    @Column(nullable = false)
+    private int productPrice;
+    @Column(nullable = false)
+    private int quantity;
+
+    public int calculateItemAmount() {
+        return this.productPrice * this.quantity;
+    }
+}
